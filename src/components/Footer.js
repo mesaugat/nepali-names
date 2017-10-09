108e3d21c3ddd11d9c422b4b4dc4abc2016aab40
Benjamin Robson <unoceptik@gmail.com>
2017-10-09 22:46:13 +0300

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 /**
  * Footer Component.
  */
-const Footer = () =>
-  <footer className="page-footer teal darken-3">
+const Footer = () => (
+  <footer className="page-footer teal darken-3" id="footer">
     <div className="footer-copyright">
       <div className="container">
         <i className="fa fa-2fa-copyright" aria-hidden="true" />
@@ -17,6 +17,7 @@ const Footer = () =>
         </a>
       </div>
     </div>
-  </footer>;
+  </footer>
+);
 
 export default Footer;
