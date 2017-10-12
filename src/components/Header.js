96e34b8978e6b99529e2849f31b82106f0017682
Saugat Acharya <mesaugat@gmail.com>
2017-10-12 22:53:43 +0545

    Add GitHub icon in header and remove unused styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,28 @@ import React from 'react';
 /**
  * Header Component.
  */
-const Header = () =>
+const Header = () => (
   <nav>
     <div className="nav-wrapper teal darken-3">
       <div className="container">
         <a href="/nepali-names" className="brand-logo">
           Nepali Names
         </a>
+        <a
+          className="grey-text text-lighten-4 right"
+          href="https://github.com/mesaugat/nepali-names"
+        >
+          <i
+            className="fa fa-github material-icons"
+            title="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-hidden="true"
+          />
+        </a>
       </div>
     </div>
-  </nav>;
+  </nav>
+);
 
 export default Header;
