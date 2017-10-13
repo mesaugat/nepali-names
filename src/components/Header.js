560cf9caf1a2844008fbad85ef08b587e7e6203e
Saugat Acharya <mesaugat@gmail.com>
2017-10-13 22:33:20 +0545

    Add target _blank to ahref

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  * Header Component.
  */
-const Header = () => (
+const Header = () =>
   <nav>
     <div className="nav-wrapper teal darken-3">
       <div className="container">
@@ -13,18 +13,17 @@ const Header = () => (
         <a
           className="grey-text text-lighten-4 right"
           href="https://github.com/mesaugat/nepali-names"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <i
             className="fa fa-github material-icons"
             title="GitHub"
-            target="_blank"
-            rel="noopener noreferrer"
             aria-hidden="true"
           />
         </a>
       </div>
     </div>
-  </nav>
-);
+  </nav>;
 
 export default Header;
